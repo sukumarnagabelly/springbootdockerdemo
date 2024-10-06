d8b63717bfcd8c6e5516d927fca7fdcea010ee75
SUKUMAR <sukumarnagabelly@gmail.com>
2024-10-06 10:26:01 +0530

    hii

---
+++ springbootdockerdemo/src/main/java/com/neotericdockerlearning/springbootdockerdemo/SpringbootdockerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.neotericdockerlearning.springbootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdockerdemoApplication.class, args);
+	}
+
+}
